aa5fcd629a63900b8df8ac8f558246429c27a9b8
Douglas <nkubitudouglas@gmail.com>
2023-06-29 22:04:29 +0300

    feat: Initialize App

--- livestock_marketing/livestock_marketing/doctype/livestock_registration/livestock_registration.js
+++ livestock_marketing/livestock_marketing/doctype/livestock_registration/livestock_registration.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Douglas Nkubitu and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Livestock', {
+frappe.ui.form.on('Livestock Registration', {
 	// refresh: function(frm) {
 
 	// }
