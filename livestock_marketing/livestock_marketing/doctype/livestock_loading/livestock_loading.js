abf3e01a6af2dd8137f29106b9446b607e76e51b
Douglas <nkubitudouglas@gmail.com>
2023-06-30 23:38:14 +0300

    feat: update child table details

--- livestock_marketing/livestock_marketing/doctype/livestock_loading/livestock_loading.js
+++ livestock_marketing/livestock_marketing/doctype/livestock_loading/livestock_loading.js
@@ -6,5 +6,23 @@ frappe.ui.form.on('Livestock Loading', {
         if (frm.is_new()) {
             frm.set_value('posting_time', frappe.datetime.now_time());
         }
-    }
+    },
+    //Get livestock details
+    livestock_registration: function (frm) {
+        if (frm.doc.livestock_registration) {
+            frm.clear_table('livestock_loading');
+            frappe.model.with_doc('Livestock Registration', frm.doc.livestock_registration, function () {
+                let source_doc = frappe.model.get_doc('Livestock Registration', frm.doc.livestock_registration);
+                $.each(source_doc.livestock_details, function (index, source_row) {
+					const target_row = frm.add_child('livestock_loading');
+                    target_row.ear_tag = source_row.ear_tag;
+                    target_row.shape = source_row.shape;
+                    target_row.cattle_brand = source_row.cattle_brand;
+                    target_row.breed = source_row.breed;
+                    target_row.gender = source_row.gender;
+                    frm.refresh_field('livestock_loading');
+                });
+            });
+        }
+    },
 });
