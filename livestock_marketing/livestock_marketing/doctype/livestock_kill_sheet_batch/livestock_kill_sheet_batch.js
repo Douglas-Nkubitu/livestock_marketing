55b1780decff24b2dcbcfcf78d8cd1a405e7a800
Douglas <nkubitudouglas@gmail.com>
2023-08-06 00:52:44 +0300

    feat: create journal entry button

--- livestock_marketing/livestock_marketing/doctype/livestock_kill_sheet_batch/livestock_kill_sheet_batch.js
+++ livestock_marketing/livestock_marketing/doctype/livestock_kill_sheet_batch/livestock_kill_sheet_batch.js
@@ -2,6 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Livestock Kill Sheet Batch', {
+    refresh: function(frm) {
+        if (frm.doc.docstatus === 1) {
+            frm.add_custom_button(__('Create Journal Entry'), function() {
+                frm.events.create_journal_entry_from_js(frm);
+            }, __('Create Journal Entry'));
+        }
+    },
+    create_journal_entry_from_js: function(frm) {
+        frappe.call({
+            method: 'livestock_marketing.livestock_marketing.doctype.livestock_kill_sheet_batch.livestock_kill_sheet_batch.create_journal_entry',
+            args: {
+                kill_sheet_batch: frm.doc.name
+            },
+            callback: function(response) {
+                // Optional: Handle the response or show a message
+                frappe.msgprint('Journal Entry created successfully!');
+                // Optional: Refresh the form to update the Journal Entry link field
+                frm.reload_doc();
+            }
+        });
+    }
+});
+
+frappe.ui.form.on('Livestock Kill Sheet Batch', {
     //selecting and adding livestock kill sheet in child table
     get_livestock_kill_sheet: function (frm) {
 		frappe.call({
@@ -22,7 +46,7 @@ frappe.ui.form.on('Livestock Kill Sheet Batch', {
 		netAmount += row.net_amount;
         });
         // Set the net amount in the field
-        frm.set_value('net_amount', netAmount);
+        frm.set_value('amount', netAmount);
 
         //Calculate Total Deduction
         var totalDeductions = 0;
@@ -31,7 +55,7 @@ frappe.ui.form.on('Livestock Kill Sheet Batch', {
 			totalDeductions += row.total_deductions;
 			});
         // Set the total deductions in the field
-        frm.set_value('total_deductions', totalDeductions);
+        frm.set_value('deductions', totalDeductions);
 
         //Calculate Total Payable Amount
         var totalPayable =0;
@@ -40,6 +64,6 @@ frappe.ui.form.on('Livestock Kill Sheet Batch', {
 		totalPayable += row.total_amount_payable;
         })
         // Set the total payable in the field
-        frm.set_value('total_amount_payable', totalPayable);
+        frm.set_value('amount_payable', totalPayable);
 	}
 });
