a87e81c5752fb2f83a2bfc5724d5e605d32d7626
Douglas <nkubitudouglas@gmail.com>
2023-08-06 00:53:16 +0300

    feat: create journal entry from Kill sheet batch

--- livestock_marketing/livestock_marketing/doctype/livestock_kill_sheet_batch/livestock_kill_sheet_batch.py
+++ livestock_marketing/livestock_marketing/doctype/livestock_kill_sheet_batch/livestock_kill_sheet_batch.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2023, Douglas Nkubitu and contributors
 # For license information, please see license.txt
 
+from frappe.utils import get_url_to_form
 import frappe
 from frappe import _
 from frappe.model.document import Document
@@ -82,3 +83,59 @@ def get_livestock_kill_sheet(self):
     """.format(lksb_filter), params, as_dict=True)
     
     return submitted_livestock_kill_sheet
+
+@frappe.whitelist()
+def create_journal_entry(kill_sheet_batch):
+	# Retrieve child table records from "Livestock Kill Sheet Batch Details"
+	child_records = frappe.get_doc("Livestock Kill Sheet Batch", kill_sheet_batch)
+
+	# Calculate the total_amount_payable from debtors
+	total_amount = sum(child_record.total_amount_payable for child_record in child_records.get("livestock_kill_sheet_batch"))
+
+	# Store the parent_docname for later use
+	parent_docname = child_records.name
+
+	# Create a new Journal Entry
+	journal_entry = frappe.new_doc("Journal Entry")
+	journal_entry.voucher_type = "Journal Entry"
+	journal_entry.posting_date = frappe.utils.today()
+	journal_entry.company = frappe.defaults.get_defaults().get("company")
+
+	# Create a new Journal Entry account row for "Cash - S"
+	cash_account_row = frappe.new_doc("Journal Entry Account")
+	cash_account_row.account = "Cash - S"  # Replace with the appropriate account name
+	cash_account_row.credit_in_account_currency = total_amount
+
+	# Append the "Cash - S" account row to the Journal Entry
+	journal_entry.append("accounts", cash_account_row)
+	for child_record in child_records.get("livestock_kill_sheet_batch"):
+		farmer_name = child_record.farmer_name
+		total_amount_payable = child_record.total_amount_payable
+
+		# Create a new Journal Entry account row for "Debtors - S" linked to the farmer
+		debtor_account_row = frappe.new_doc("Journal Entry Account")
+		debtor_account_row.account = "Debtors - S"  # Replace with the appropriate account name
+		debtor_account_row.party_type = "Customer"
+		debtor_account_row.party = farmer_name
+		debtor_account_row.user_remark = f"Journal Entry created from {parent_docname}"
+		debtor_account_row.debit_in_account_currency = total_amount_payable
+
+		# Append the "Debtors - S" account row to the Journal Entry
+		journal_entry.append("accounts", debtor_account_row)
+
+		# # Print the data before creating the Journal Entry (optional)
+		# frappe.msgprint(f"Farmer Name: {farmer_name}, Total Amount Payable: {total_amount_payable}")
+
+	journal_entry.cheque_no = ""  
+	journal_entry.cheque_date = ""
+	journal_entry.user_remark = f"Journal Entry created from {parent_docname} Batch"
+	journal_entry.remark = f"Journal Entry created from {parent_docname} Batch"
+	journal_entry.insert(ignore_permissions=True)
+	journal_entry.save()
+	jv_url = get_url_to_form(journal_entry.doctype, journal_entry.name)
+	jv_msgprint = f"Journal Entry Created <a href='{jv_url}'>{journal_entry.name}</a>"
+	frappe.msgprint(_(jv_msgprint))
+
+	return journal_entry.name
+
+
