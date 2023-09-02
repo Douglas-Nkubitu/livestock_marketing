55ac119858c113ac4fe85ab8a9ca19d812a0e0a2
Douglas <nkubitudouglas@gmail.com>
2023-09-02 10:46:32 +0300

    fix: set account type filter as Payable

--- livestock_marketing/api/member.js
+++ livestock_marketing/api/member.js
@@ -3,7 +3,7 @@ frappe.ui.form.on("Member", {
         frm.set_query('account', 'accounts', function(doc, cdt, cdn) {
             var d  = locals[cdt][cdn];
             var filters = {
-                'account_type': 'Receivable',
+                'account_type': 'Payable',
                 'company': d.company,
                 "is_group": 0
             };
